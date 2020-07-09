2d0e481aa2d2098219d2bfe7b100f2ad53fdcc8b
supriti <sharmasupriti167@gmail.com>
2020-07-09 14:05:17 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/data-service.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import { GlobalDataSummary } from '../models/global-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  private globalDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-03-2020.csv';
+  constructor(private http : HttpClient ) { }
+
+  getGlobalData(){
+    return this.http.get(this.globalDataUrl, {responseType:'text'}).pipe(
+      map(result => {
+        let data: GlobalDataSummary[]=[]
+        let rows = result.split(`\n`);
+        
+        rows.forEach(row=>{
+          let cols = row.split(/,(?=\S)/)
+          data.push({
+            country : cols[3], confirmed : +cols[7], deaths : +cols[8], recovered : +cols[9], active : +cols[10],
+          })
+        })
+        console.log(data);
+        return [];
+      })
+    )
+  }
+}
